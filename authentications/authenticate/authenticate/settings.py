1eec81675df09f6c2d54f1772bd5acd63e75ebcc
Niharranjanterralogic <106967351+Niharranjanterralogic@users.noreply.github.com>
2022-06-18 23:36:28 +0530

    commit

--- authentications/authenticate/authenticate/settings.py
+++ authentications/authenticate/authenticate/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 # STATICFILES_DIRS=(
 #     os.path.join(BASE_DIR,'static'),
 # )
